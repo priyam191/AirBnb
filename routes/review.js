7c3157edaa4431b8ba98604f0669f87ef145c0d7
priyam <ghoshpriyam191@gmail.com>
2024-09-02 23:40:05 +0530

    updated

--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const expressError = require("../utils/expressError.js");
-const { listingSchema,reviewSchema } = require("../schema.js");
+const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing= require("../models/listing.js");
 
@@ -20,9 +20,9 @@ const validateReview = (req, res, next) => {
 
 router.post("/", validateReview, wrapAsync(async(req,res) =>{
     let listing = await Listing.findById(req.params.id);
-    console.log(req.params.id);
+   
     let newReview = new Review(req.body.review);
-    console.log(listing);
+    
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
